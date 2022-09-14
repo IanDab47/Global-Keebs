be7eb821b7ce5d8b36de69f3e592fa2998c64e1d
iandab47 <dabinett.ian@gmail.com>
2022-09-14 18:27:11 -0500

    added filter system for listing search

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,15 +4,31 @@ const db = require('../models')
 
 const typeArr = ['selling, buying, stores']
 
-router.get('/:type', (req, res) => {
-  const listingType = req.params.type
-  const trueType = typeArr.filter(type => listingType === type)
+router.get('/', (req, res) => {
+  const errorMsg = req.query.error || null
+  const filterType = req.query.filter || null
+  const trueType = typeArr.filter(type => filterType === type)
 
   if(trueType) {
-    res.render('listings/show', { webpage: listingType })
+    res.render('listings/list', { errorMsg })
   } else {
     const falseType = 'That listing does not exist.'
     res.redirect(`/?message=${falseType}`)
+  }
+})
+
+router.get('/:page_id', async (req, res) => {
+  try {
+    const listing = db.listing.findOne({
+      where: {
+        page_id: page_id
+      }
+    })
+    res.render('listings/show', { listing })
+  } catch(err) {
+    console.warn(err)
+    const badListing = 'Listing does not exist.'
+    res.redirect(`/listings/?error=${badListing}`)
   }
 })
 
