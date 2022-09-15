317a0b514bb7b02575c9a4ede90a6328795d2aae
iandab47 <dabinett.ian@gmail.com>
2022-09-15 16:02:45 -0500

    added filter for self_text

--- public/js/functions.js
+++ public/js/functions.js
@@ -24,69 +24,81 @@ const ageCheck = (timestamp) => {
   return timestamp * 1000 >= Date.now() - twoMonths ? true : false
 }
 
+const filterSelfText = (self_text) => {
+  // Remove 1st set of brackets and its contents 
+  const re_brackets = /\[([^\]]+)\]/
+  const re_paren = /\(([^)]+)\)/g
+  self_text = self_text.replace(re_brackets, "")
+  console.log(self_text)
+}
+
 const createListing = async (listing) => {
-
-  // grab immediate values
-  const author = listing.author
-  const author_ref = listing.author_fullname
-  const created_utc = listing.created_utc
-  const downs = listing.downs
-  const flair_text = listing.link_flair_text.toUpperCase() || 'N/A'
-  const page_id = listing.id || null
-  const page_name = listing.name
-  const self_text = listing.selftext
-  const title = listing.title
-  const ups = listing.ups
-  const upvote_ratio = listing.upvote_ratio
-  const url = listing.url
-
-  console.log(flair_text)
-
-  // grab first bracket for location
-  const re_local = /\[([^\]]+)\]/
-  let location = ''
-  const titleText = re_local.exec(title) || ['ha', null ]
-  titleText[1] ? location = titleText[1] : location = 'N/A'
-
-  // grab first link and filter for imgur link
-  const re_imgur = /\(([^)]+)\)/g
-  let timestamp = ''
-  const listingText = re_imgur.match(listing.selftext) || ['ha', 'no timestamp']
-  listingText[1].includes('imgur') ? timestamp = listingText[1] : timestamp = 'no timestamp'
-
-  const [newListing, created] = await db.listing.findOrCreate({
-    where: {
-      page_id: page_id,
-    },
-    defaults: {
-      author: author,
-      author_ref: author_ref,
-      created_utc: created_utc,
-      downs: downs,
-      flair_text: flair_text,
-      location: location,
-      page_name: page_name,
-      self_text: self_text,
-      timestamp: timestamp,
-      title: title,
-      ups: ups,
-      upvote_ratio: upvote_ratio,
-      url: url
-    }
-  })
-
-  return created
-  // const someText = [
-  //   listing.selftext
-  // ]
-  // const re_newLine = /.*(?!:\n)/gm
-  // const lineOne = re_newLine.exec(listing.selftext) || ['', 'no line']
-  // const re_imgur = /\(([^)]+)\)/gm
-  // const [zero, txt] = re_imgur.exec(lineOne) || ['', 'no timestamp']
-  // // console.log(txt[1])
-  // console.log(txt)
-  // console.log(someText)
-  // console.log('------ NEW LISTING ------')
+  try {
+    // grab immediate values
+    const author = listing.author
+    const author_ref = listing.author_fullname
+    const created_utc = listing.created_utc
+    const downs = listing.downs
+    const flair_text = listing.link_flair_text.toUpperCase() || 'N/A'
+    const page_id = listing.id || null
+    const page_name = listing.name
+    const self_text = listing.selftext
+    const title = listing.title
+    const ups = listing.ups
+    const upvote_ratio = listing.upvote_ratio
+    const url = listing.url
+  
+    // grab first bracket for location
+    const re_local = /\[([^\]]+)\]/
+    let location = ''
+    const titleText = re_local.exec(title) || ['ha', null ]
+    titleText[1] ? location = titleText[1] : location = 'N/A'
+  
+    // grab first link and filter for imgur link
+    const re_imgur = /\(([^)]+)\)/g
+    let timestamp = ''
+    const listingText = self_text.match(re_imgur) || ['ha', 'no timestamp']
+    // console.log(listingText)
+    filterSelfText(self_text)
+    console.log('------ NEW LISTING ------')
+    // listingText[1].includes('imgur') ? timestamp = listingText[1] : timestamp = 'no timestamp'
+  
+    // Find or create new model with necessary listing data
+    const [newListing, created] = await db.listing.findOrCreate({
+      where: {
+        page_id: page_id,
+      },
+      defaults: {
+        author: author,
+        author_ref: author_ref,
+        created_utc: created_utc,
+        downs: downs,
+        flair_text: flair_text,
+        location: location,
+        page_name: page_name,
+        self_text: self_text,
+        timestamp: timestamp,
+        title: title,
+        ups: ups,
+        upvote_ratio: upvote_ratio,
+        url: url
+      }
+    })
+  
+    return created
+    // const someText = [
+    //   listing.selftext
+    // ]
+    // const re_newLine = /.*(?!:\n)/gm
+    // const lineOne = re_newLine.exec(listing.selftext) || ['', 'no line']
+    // const re_imgur = /\(([^)]+)\)/gm
+    // const [zero, txt] = re_imgur.exec(lineOne) || ['', 'no timestamp']
+    // // console.log(txt[1])
+    // console.log(txt)
+    // console.log(someText)
+  } catch(err) {
+    console.warn(err)
+  }
 }
 
 const addListings = async (url) => {
@@ -96,36 +108,34 @@ const addListings = async (url) => {
   let allListings = await axios.get(url)
     .then(response => {
       response.data.data.children.map( async (listing, index) => {
-        // let newListing = null
-        // Add listings to array
-        listings.push(listing.data)
-        if(listing.data.link_flair_text !== null || listing.data.link_flair_text !== undefined) {
-          await createListing(listing.data)
-        }
-
-        // if(newListing) {
-          // check for final possible listing
-          if(index === response.data.data.dist - 1 && response.data.data.after === null || listings.length === 1000) {
-          // if(index === response.data.data.dist - 1) {
-            console.log('done!')
-  
-            // listings = filterListings(listings)
-            // console.log('filtered!')
-  
-            // listings.map((listing, index) => {
-            //   listings[index] = createListing(listing)
-            // })
-            return
-          } else if (index === response.data.data.dist - 1) { // Checks for end of page
-            // Update url to load next page
-            after = response.data.data.after
-            url = `https://www.reddit.com/r/mechmarket/new/.json?after=${after}`
-  
-            // Recursively append listings
-            // console.log(response.data.data.after, listings.length)
-            return await addListings(url)
+        try {
+          const newListing = null
+          // let newListing = null
+          // Add listings to array
+          listings.push(listing.data)
+          if(listing.data.link_flair_text !== null || listing.data.link_flair_text !== undefined) {
+            newListing = await createListing(listing.data)
           }
-        // }
+          if(newListing) {
+            // check for final possible listing
+            if(index === response.data.data.dist - 1 && response.data.data.after === null || listings.length === 1000) {
+            // if(index === response.data.data.dist - 1) {
+              console.log('done!')
+              return
+            } else if (index === response.data.data.dist - 1) { // Checks for end of page
+              // Update url to load next page
+              after = response.data.data.after
+              url = `https://www.reddit.com/r/mechmarket/new/.json?after=${after}`
+    
+              // Recursively append listings
+              // console.log(response.data.data.after, listings.length)
+              return await addListings(url)
+            }
+          }
+        } catch(err) {
+
+        }
+        
       })
     })
     .catch(console.warn)
@@ -154,10 +164,14 @@ const filterListings = (listings) => {
 }
 
 const checkListings = async () => {
-  // Declare necessary variables
-  let listings = await addListings(mmURL)
-  console.log(listings)
-  // listings = filterListings(listings)
+  try {
+    // Declare necessary variables
+    let listings = await addListings(mmURL)
+    console.log(listings)
+    // listings = filterListings(listings)
+  } catch(err) {
+    console.log(err)
+  }
 }
 
 // export necessary functions
